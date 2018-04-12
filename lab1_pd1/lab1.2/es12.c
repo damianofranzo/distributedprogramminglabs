3ea8d6bd2a0a56fb64484ed75454f6f64a6ac572
damianofranzo <s252854@studenti.polito.it>
2018-04-12 23:38:44 +0200

    diddin

--- lab1_pd1/lab1.2/es12.c
+++ lab1_pd1/lab1.2/es12.c
@@ -1,10 +1,17 @@
+
+#include <sys/types.h>
 #include <sys/socket.h>
+#include "sockwrap.h"
 
-#include "socketwrap.h"
 
 
-int main(int argc, char argv [][]){
-    int socketc;
+//it has to be declared outside, to be linked with the *proo_name var of sockwrap.c
+char *prog_name;
+
+int main(int argc, char** argv){
+    int socketc=Socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
+
+
 
 }
 
