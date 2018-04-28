ded2aeb72dc020a3f9afef06b6d6dfb64d8c2a19
danzio <s252854@studenti.polito.it>
2018-04-28 18:02:22 +0200

    it works now

--- lab2_dp1/lab2.3/source/server1/server1_main.c
+++ lab2_dp1/lab2.3/source/server1/server1_main.c
@@ -10,6 +10,7 @@
 //#include <sys/socket.h>
 #include <netinet/in.h>
 #include <sys/stat.h>
+#include <fcntl.h>
 
 
 #include "../errlib.h"
@@ -31,6 +32,14 @@
 
 //#define BUFF_SIZE 1400
 
+
+/*To do:
+ * good stdout
+ * wrap into functions
+ * use more marco
+ * send file on 2mb at time
+ * good numbers
+ * */
 
 char *prog_name;
 
@@ -87,43 +96,55 @@ int main (int argc, char *argv[])
     Listen(listening_socket,LQ);
     printf("Listening on %d\n",listening_socket);
 
+    //i can assume a request less than integer limit of byte
+    int request_size;
+
+
+    //variable to handle connection
+    int keep_conn_on = 1;
+    int mess ;
+
+    //variables to handles file tranfer
+    int fd;
+    char filename[N];
+    char *buffer_file=NULL;
+    struct stat fileinformation;
+    size_t file_size,total_size;
+
+    char file_size_string[4],time_stamp_string[4];
+
     while(flag) {
         //Waiting for client connections
         connection_socket = Accept(listening_socket,(struct sockaddr*) &caddr, &caddrlen);
-        printf("Connetion accepted on %d\n",connection_socket);
         if (connection_socket < -1) {
             printf("Error in Accept function\n");
             continue;
         }
+        printf("Connetion accepted on %d\n",connection_socket);
+        keep_conn_on=1;
 
-        //i can assume a request less than integer limit of byte
-        int request_size=(int)Recv(connection_socket, buffer, N, 0);
-        printf("Buffer received:%s\n",buffer);
 
-        //variable to handle connection
-        int keep_conn_on = 1;
-        int mess = getmessage(buffer);
-
-        //variables to handles file tranfer
-        int fd;
-        char filename[N];
-        char *buffer_file=NULL;
-        struct stat fileinformation;
-        size_t file_size,total_size;
-
-        char file_size_string[4],time_stamp_string[4];
 
 
 
         while (keep_conn_on) {
+            //waiting for commands
+            request_size=(int)Recv(connection_socket, buffer,1000, 0);
+            mess=getmessage(buffer);
+            printf("Buffer received:%s\n",buffer);
             switch (mess) {
                 case PROT_GET:
-                    getfilename(buffer,filename,request_size);
+                    if(getfilename(buffer,filename,request_size)<0){
+                        printf("Request too short,closing connection\n");
+                        keep_conn_on=0;
+                        mess=PROT_ERR;
+                        break;
+                    }
                     printf("Client requiring file: %s\n",filename);
                     //fd=open(filename,"r");
 
                     //get information about file
-                    if(stat(buffer,&fileinformation)<0){
+                    if(stat(filename,&fileinformation)<0){
                         printf("File not found, closing connection\n");
                         keep_conn_on=0;
                         mess=PROT_ERR;
@@ -132,40 +153,93 @@ int main (int argc, char *argv[])
 
                     //i allocate a buffer of the total size of the file plus space for +okb1b2..
                     file_size=(size_t)fileinformation.st_size;
-                    total_size=file_size+13;
+                    total_size=file_size+14;
                     buffer_file=malloc(total_size);
 
                    //Reading the file and sending it
-                    read(fd,buffer_file+13,file_size);
+                    fd=open(filename,O_RDONLY);
+                    if(fd<0){
+                        printf("failed to open file\n");
+                        mess=PROT_ERR;
+                        break;
+                    }
 
-                    long_to_string(file_size_string,htonl((int)file_size));
-                    long_to_string(time_stamp_string,(int)file_size);
-                    strncpy(buffer,"+ok\r",4);
-                    strncpy(buffer+5,file_size_string,4);
-                    strncpy(buffer+9,file_size_string,4);
+                    //changed in order to send correctly all the file for each mb
+                    if(readn(fd,buffer_file+13,file_size)==file_size)
+                        printf("file read correctly\n");
+                    close(fd);
+                    printf("File Information\nfile:%s\n",buffer_file+13);
+
+                    if(long_to_string(file_size_string,htonl((int)file_size))==-1){
+                        keep_conn_on=0;
+                        break;
+                    }
+                    file_size=htonl(file_size);
+                    //memcpy(file_size_string,&file_size, sizeof(uint32_t));
+                //    printf("%s\n",file_size_string);
+                  //  printf("%d %d %d %d\n",file_size_string[0],file_size_string[1],file_size_string[2],file_size_string[3]);
+                  //  printf("problem here: %d\n",(int)fileinformation.st_mtim.tv_sec);
+
+                /*    if(long_to_string(time_stamp_string,htonl((int)fileinformation.st_mtim.tv_sec))==-1){
+                        keep_conn_on=0;
+                        break;
+                    }*/
+                    printf("Timestamp: %d\n",fileinformation.st_mtim.tv_sec);
+                    int timestamp=htonl((int)fileinformation.st_mtim.tv_sec);
+                    //memcpy(time_stamp_string,&timestamp, sizeof(uint32_t));
+
+                    //printf("what is this: ...%1s %1s %1s %1s...\n",time_stamp_string,time_stamp_string+1,time_stamp_string+2,time_stamp_string+3);
+                   // printf("%d %d %d %d\n",time_stamp_string[0],time_stamp_string[1],time_stamp_string[2],time_stamp_string[3]);
+
+                    strncpy(buffer_file,"+OK\r\n",5);
+                    memcpy(buffer_file+5,&file_size,sizeof(int));
+                    memcpy(buffer_file+9,&timestamp,sizeof(int));
+
+                    /*Send(connection_socket,&file_size,4,0);
+                    Send(connection_socket,buffer_file,5,0);
+                    Send(connection_socket,&timestamp,4,0);
+                    */
 
                     Send(connection_socket,buffer_file,total_size,0);
+                    printf("Sending %s\n",buffer_file);
+                    //Send(connection_socket,buffer_file+13,total_size-14,0);
 
-
+                    printf("File sent successfully\n");
                     break;
                 case PROT_QUIT:
                     keep_conn_on = 0;
                     printf("Client requested to quit, closing connection\n");
                     break;
                 case PROT_ERR:
-                    printf("Client sent invalid message\n");
+                    printf("Client sent invalid message, closing connection\n");
                     keep_conn_on=0;
                     Send(connection_socket,PROT_ERR_MSG,PROT_ERR_MSG_LEN,0);
                     break;
-                default:    printf("Something strange happened\n");
+                default:
+                    printf("Something strange happened\n");
                     keep_conn_on=0;
-
             }
-            Close(connection_socket);
-            if(buffer_file!=NULL)
-                free(buffer_file);
 
+            if(mess==PROT_QUIT){
+                keep_conn_on = 0;
+                printf("Client requested to quit, closing connection\n");
+            }
+            else if(mess==PROT_ERR){
+                printf("Client sent invalid message, closing connection\n");
+                keep_conn_on=0;
+                Send(connection_socket,PROT_ERR_MSG,PROT_ERR_MSG_LEN,0);
+                break;
+            }
+            else{
+                printf("Something strange happened\n");
+                keep_conn_on=0;
+            }
         }
+        free(buffer_file);
+        buffer_file=NULL;
+        Close(connection_socket);
+        if(buffer_file!=NULL)
+            free(buffer_file);
 
         // the endless loop has to be handled
         i++;
@@ -194,19 +268,25 @@ int getmessage(char *buffer){
 int getfilename(char *buf,char *filename,int request_size){
     if(filename==NULL|| buf==NULL)
         return -1;
-    strncpy(filename,buf+4,strlen(buf)-6);
-    filename[strlen(buf)-5]='\0';
+    if(request_size<6)
+        return -1;
+    strncpy(filename,buf+4,request_size-6);
+    filename[request_size-6]='\0';
     return 0;
 }
 
 int long_to_string(char *stringl, int size){
-    if(stringl==NULL)
+/*    if(stringl==NULL)
         return -1;
-    int n;
-    for(int i=0;i<4;i++){
-        n=size%256;
-        (size-n)/256;
-        stringl[i]=(char)n;
-    }
+    else if (strlen(stringl)<4)
+        return -1;*/
+    //printf("Given size %d\n",size);
+  /*  stringl[0]=(size)&0xff;
+    stringl[1]=(size>>8)&0xff;
+    stringl[2]=(size>>16)&0xff;
+    stringl[3]=(size>>24)&0xff;*/
+
+  memcpy(stringl,&size,4);
+   // printf("%d%d%d%d\n",stringl[0],stringl[1],stringl[2],stringl[3]);
     return 0;
 };
