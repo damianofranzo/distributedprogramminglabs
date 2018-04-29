18b6bd3d2472cbe58f7d96a603574f20a85f8858
danzio <s252854@studenti.polito.it>
2018-04-29 19:00:44 +0200

    test passed

--- lab2_dp1/lab2.3/source/client1/client1_main.c
+++ lab2_dp1/lab2.3/source/client1/client1_main.c
@@ -3,10 +3,181 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/select.h>
+#include <unistd.h>
+#include <netinet/in.h>
+
+
+#include "../sockwrap.h"
+#include "../errlib.h"
+
+
+#define GET_MSG "GET "
+#define GET_MSG_LEN 4
+#define QUIT_MSG "QUIT\r\n"
+#define QUIT_MSG_LEN 6
+#define END_MSG "\r\n"
+#define END_MSG_SIZE 2
+
+#define GET_SERVER_MSG "+OK\r\n"
+#define GET_SERVER_MSG_LEN 5
+#define ERR_MSG "-ERR\r\n"
+#define ERR_MSG_LEN 6
+
+
+#define MYBUFFSIZE 1400
+
+#define WAITING_TIME 30
+#define HEADER_LEN 13
+
+#define SYS_ERR -2
+#define PROT_ERR -1
+#define PROT_GET 1
 
 char *prog_name;
 
+size_t getFileSize(char *buffer);
+int getMessage(char *buffer);
+
 int main (int argc, char *argv[])
 {
-	return 0;
+    //variable to handle connections
+    int sock,quit=1;
+    struct sockaddr_in serverAddr;
+    socklen_t  fromlen;
+    struct sockaddr_in from;
+
+    //to be used for programs
+    prog_name = argv[0];
+    if(argc <3)
+        err_quit("Usage: %s <dest_IP_add> <dest_port>\n", argv[0]);
+
+    sock = Socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    fprintf(stdout,"Socket created\n");
+
+    serverAddr.sin_family    = AF_INET;
+    serverAddr.sin_port      = htons((uint16_t) atoi(argv[2]));
+    serverAddr.sin_addr.s_addr = inet_addr(argv[1]);
+
+    Connect(sock, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+    fprintf(stdout,"Connection Established with %s on port %s\n",argv[1],argv[2]);
+
+    //variable to handle file tranfer
+    size_t fileNameLength,totlength,filesize;
+    char buffer[MYBUFFSIZE],filename[50];
+    struct timeval tval;
+    fd_set cset;
+    FILE *fd;
+    int n,r,keep_conn_on=1,mess,remaining;
+
+    //temp
+    char file[MYBUFFSIZE];
+
+    //for each file given from commandline i sent request and wait for response
+    for(int i=3;i<argc;i++) {
+        if ((argv[i] == NULL)) {
+            Send(sock, QUIT_MSG, QUIT_MSG_LEN, 0);
+        } else if (strlen(argv[i]) < 1) {
+            Send(sock, QUIT_MSG, QUIT_MSG_LEN, 0);
+        } else {
+            fileNameLength = strlen(argv[i]);
+
+            //preparing buffer message
+            memcpy(buffer, GET_MSG, GET_MSG_LEN);
+            printf("buffer %s\n",buffer);
+            memcpy(buffer + GET_MSG_LEN, argv[i], fileNameLength);
+            memcpy(buffer + GET_MSG_LEN + fileNameLength,END_MSG,END_MSG_SIZE);
+
+            fprintf(stdout, "Requesting file %.8s, length of name %d\n", buffer + 4, (int)fileNameLength);
+            totlength = GET_MSG_LEN + fileNameLength + END_MSG_SIZE;
+            Send(sock, buffer, totlength, 0);
+
+            FD_ZERO(&cset);
+            FD_SET(sock, &cset);
+
+            tval.tv_sec = WAITING_TIME;
+            tval.tv_usec = 0;
+
+            if ((n = select(FD_SETSIZE, &cset, NULL, NULL, &tval)) == -1) {
+                ("select() failed\n");
+                break;
+            }
+            if (n > 0) {
+                if (FD_ISSET(sock, &cset)) {
+                    Recvfrom(sock, buffer, HEADER_LEN, 0, (struct sockaddr *) &from, &fromlen);
+                    mess=getMessage(buffer);
+                    switch(mess){
+                        case PROT_GET:
+                            //get size and timestamp
+                            filesize=getFileSize(buffer);
+                            remaining=filesize;
+                            printf("Remaining: %d\n",(int)filesize);
+                            fd=fopen(argv[i],"w+");
+                            while(remaining>0){
+                                r=Recvfrom(sock, buffer, MYBUFFSIZE, 0, (struct sockaddr *) &from, &fromlen);
+                                printf("buffer %s\n",buffer);
+                                memcpy(file+(filesize-remaining),buffer,r);
+                                remaining-=r;
+                                fwrite(buffer,1,r,fd);
+                            }
+                            fclose(fd);
+                            //temp
+                            printf("file %s",file);
+
+                            break;
+                        case PROT_ERR:
+                            fprintf(stderr,"error prot\n");
+                            quit=0;
+                            break;
+                        case SYS_ERR:
+                            fprintf(stderr,"system error\n");
+                            quit=0;
+                            break;
+                        default:
+                            mess=PROT_ERR;
+
+                    }
+                }
+                else
+                    fprintf(stderr, "Error in select\n");
+
+
+
+            }
+            else
+                fprintf(stderr, "No response from the server, closing connection");
+        }
+    }
+    //if no problem happened, send quit
+    if(quit) Send(sock, QUIT_MSG, QUIT_MSG_LEN, 0);
+    Close(sock);
+    //send quit
+    return 0;
+}
+
+int getMessage(char *buffer){
+    if(buffer==NULL){
+        return SYS_ERR;
+    }
+    else if(strncmp(buffer,ERR_MSG,ERR_MSG_LEN)==0){
+        return PROT_ERR;
+    }
+    else if(strncmp(buffer,GET_SERVER_MSG,GET_SERVER_MSG_LEN)==0){
+        return PROT_GET;
+    } else{
+        return SYS_ERR;
+    }
+}
+
+size_t getFileSize(char *buffer) {
+    if (buffer ==NULL) {
+        return 0;
+    }
+    size_t filesize;
+
+    memcpy(&filesize,buffer+5, sizeof(int));
+    filesize=ntohl(filesize);
+    return filesize;
+
 }
--- lab2_dp1/lab2.3/source/server1/server1_main.c
+++ lab2_dp1/lab2.3/source/server1/server1_main.c
@@ -7,7 +7,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
-//#include <sys/socket.h>
 #include <netinet/in.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -17,7 +16,6 @@
 #include "../sockwrap.h"
 
 
-#define N 50
 #define LQ 10
 
 
@@ -32,8 +30,11 @@
 #define SERV_ERR 1
 
 #define MYBUFSIZE 1400
+#define MYBUFSIZE2 1000
+#define MYFILENAMESIZE 150
 
 
+#define HEADER_LEN 13
 /*To do:
  * good stdout
  * */
@@ -56,23 +57,23 @@ int main (int argc, char *argv[])
     struct sockaddr_in saddr,caddr;
     socklen_t  caddrlen;
 
-    char buffer[N];
-    int i=0,flag=1;
+    char buffer[MYBUFSIZE2];
+    int flag=1;
 
     //sockwrap.h has to know prog_name declared with extern
     prog_name=argv[0];
-
 
     //convert the char* port into a integer one
     port=atoi(argv[1]);
 
     //checking the correctness of the port
-    if(port>65556){
-        printf("Error -> Insert a correct port\n");
+    if(port>65536){
+        fprintf(stderr,"Error: Insert a correct port, it should be a number less then 65536\n");
         return -1;
     }
 
     listening_socket=Socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
+    fprintf(stdout,"Socket Created\n");
 
     //assigning the sockaddr_in structure
     memset(&saddr,0,sizeof(saddr));
@@ -80,76 +81,75 @@ int main (int argc, char *argv[])
     saddr.sin_port = htons(((short)port));
     saddr.sin_addr.s_addr = htonl(INADDR_ANY);
 
+    //if the port was closed in a bad way, it should make the port reusable
     int enable = 1;
     if (setsockopt(listening_socket, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0)
         printf("setsockopt(SO_REUSEADDR) failed\n");
 
     Bind(listening_socket,(struct sockaddr*) &saddr, sizeof(saddr));
-
-
-
-    printf("Socket created\n");
+    fprintf(stdout, "Bind executed with success\n");
 
     Listen(listening_socket,LQ);
-    printf("Listening on %d\n",listening_socket);
+    fprintf(stdout,"Listening on %d\n",listening_socket);
 
     //i can assume a request less than integer limit of byte
     int request_size;
 
 
-    //variable to handle connection
-    int keep_conn_on = 1;
-    int mess ;
+    //variables to handle connection
+    int mess,keep_conn_on = 1;
 
-    //variables to handles file tranfer
-    int fd;
-    char filename[N];
+    //variables to handles file transfer
+    int fd,timestamp;
+    char filename[MYFILENAMESIZE];
     char *buffer_file=NULL;
     struct stat fileinformation;
     size_t file_size,total_size;
 
-    char file_size_string[4],time_stamp_string[4];
 
     while(flag) {
         //Waiting for client connections
         connection_socket = Accept(listening_socket, (struct sockaddr *) &caddr, &caddrlen);
         if (connection_socket < -1) {
-            printf("Error in Accept function\n");
+            fprintf(stderr,"Error in Accept function\n");
             continue;
         }
-        printf("Connetion accepted on %d\n", connection_socket);
+        fprintf(stdout,"Connection accepted on %d\n", connection_socket);
+
+        //i assume the connection to be always up, until the client request to quit. When
+        //the client want to stop the connection, this variable is set to 0
         keep_conn_on = 1;
 
 
         while (keep_conn_on) {
-            //waiting for commands
-            request_size = (int) Recv(connection_socket, buffer, 1000, 0);
-            mess = getmessage(buffer);
-            printf("Buffer received:%s\n", buffer);
 
+            //Wating messages from the client connected
+            request_size = (int) Recv(connection_socket, buffer, MYBUFSIZE2, 0);
+
+            //i inspect first character of the message, in order to get the type of comand
+            mess = getmessage(buffer);
             switch (mess) {
                 case PROT_GET:
                     if (getfilename(buffer, filename, request_size) < 0) {
-                        printf("Request too short,closing connection\n");
+                        fprintf(stdout,"Problems with buffer, closing connection\n");
                         keep_conn_on = 0;
                         mess = PROT_ERR;
                         break;
                     }
-                    printf("Client requiring file: %s\n", filename);
-                    //fd=open(filename,"r");
+                    fprintf(stdout,"Client requiring file: %s\n", filename);
 
                     //get information about file
                     if (stat(filename, &fileinformation) < 0) {
-                        printf("File not found, closing connection\n");
+                        fprintf(stdout,"File not found, closing connection\n");
                         keep_conn_on = 0;
                         mess = PROT_ERR;
                         break;
                     }
 
-                    //i allocate a buffer of the total size of the file plus space for +okb1b2..
+                    //i allocate a buffer of the size of the first information (+okb1b2..)
                     file_size = (size_t) fileinformation.st_size;
-                    total_size = file_size + 14;
-                    buffer_file = malloc(14*sizeof(char));
+                    total_size = file_size + 13;
+                    buffer_file = malloc(13*sizeof(char));
 
                     //Reading the file and sending it
                     fd = open(filename, O_RDONLY);
@@ -160,99 +160,45 @@ int main (int argc, char *argv[])
                     }
 
                     //changed in order to send correctly all the file for each mb
-
-
                     file_size = htonl(file_size);
-                    int timestamp = htonl((int) fileinformation.st_mtim.tv_sec);
+                    timestamp = htonl((int) fileinformation.st_mtim.tv_sec);
 
                     strncpy(buffer_file, "+OK\r\n", 5);
                     memcpy(buffer_file + 5, &file_size, sizeof(int));
                     memcpy(buffer_file + 9, &timestamp, sizeof(int));
 
-                    Send(connection_socket, buffer_file,14, 0);
+                    Send(connection_socket, buffer_file,HEADER_LEN, 0);
                     if(sendfilemyprot(connection_socket,fd)<0){
-                        printf("Error in reading file, closing connection\n");
+                        fprintf(stdout,"Error in reading file, closing connection\n");
                         keep_conn_on=0;
                         Close(fd);
                         break;
                     }
                     close(fd);
                     break;
-           /*     case PROT_GET:
-                    if (getfilename(buffer, filename, request_size) < 0) {
-                        printf("Request too short,closing connection\n");
-                        keep_conn_on = 0;
-                        mess = PROT_ERR;
-                        break;
-                    }
-                    printf("Client requiring file: %s\n", filename);
-                    //fd=open(filename,"r");
-
-                    //get information about file
-                    if (stat(filename, &fileinformation) < 0) {
-                        printf("File not found, closing connection\n");
-                        keep_conn_on = 0;
-                        mess = PROT_ERR;
-                        break;
-                    }
-
-                    //i allocate a buffer of the total size of the file plus space for +okb1b2..
-                    file_size = (size_t) fileinformation.st_size;
-                    total_size = file_size + 14;
-                    buffer_file = malloc(total_size);
-
-                    //Reading the file and sending it
-                    fd = open(filename, O_RDONLY);
-                    if (fd < 0) {
-                        printf("failed to open file\n");
-                        mess = PROT_ERR;
-                        break;
-                    }
-
-                    //changed in order to send correctly all the file for each mb
-                    if (readn(fd, buffer_file + 13, file_size) == file_size)
-                        printf("file read correctly\n");
-                    close(fd);
-                    printf("File Information\nfile:%s\n", buffer_file + 13);
-
-                    file_size = htonl(file_size);
-                    int timestamp = htonl((int) fileinformation.st_mtim.tv_sec);
-
-                    strncpy(buffer_file, "+OK\r\n", 5);
-                    memcpy(buffer_file + 5, &file_size, sizeof(int));
-                    memcpy(buffer_file + 9, &timestamp, sizeof(int));
-
-                    printf("Sending %s\n", buffer_file);
-                    Send(connection_socket, buffer_file, total_size, 0);
-                    printf("File sent successfully\n");
-                    break;*/
                 case PROT_QUIT:
                     keep_conn_on = 0;
                     printf("Client requested to quit, closing connection\n");
                     break;
                 default:
-                    printf("Something strange happened\n");
+                    //mess is set to PROT_ERR
                     mess = PROT_ERR;
-                    keep_conn_on = 0;
             }
 
             //if some errors happened
             if (mess == PROT_ERR) {
-                printf("Client sent invalid message, closing connection\n");
+                fprintf(stdout,"Client sent invalid message, closing connection\n");
                 keep_conn_on = 0;
                 Send(connection_socket, PROT_ERR_MSG, PROT_ERR_MSG_LEN, 0);
             }
+
+            //free the buffer, only if used. (E.G. Client could send only a quit request)
             if (buffer_file != NULL) {
                 free(buffer_file);
-                buffer_file == NULL;
+                buffer_file = NULL;
             }
         }
         Close(connection_socket);
-
-        // the endless loop has to be handled
-            i++;
-            if (i == 5)
-                flag = 0;
         }
 
 
